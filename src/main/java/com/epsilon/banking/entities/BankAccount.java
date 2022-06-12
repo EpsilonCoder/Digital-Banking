16df6bd19d6916fc87d155c3b037b01a307f06ce
hacker1998-thosiba <sowcheikh086@gmail.com>
2022-06-12 02:00:33 +0000

    Teste de la strategie TablePerClass

--- src/main/java/com/epsilon/banking/entities/BankAccount.java
+++ src/main/java/com/epsilon/banking/entities/BankAccount.java
@@ -23,7 +23,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 //@DiscriminatorColumn(name = "Type",length = 4,discriminatorType = DiscriminatorType.STRING)
 public class BankAccount {
 	@Id
